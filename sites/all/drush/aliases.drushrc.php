6b1b7b356b3964e3ba82f1c6a30d11a2d46c3fc4
Chris Morris <cmorris@ccistudios.com>
2013-09-25 16:27:50 -0400

    More edits
    Added ckeditor
    Updated features

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -11,3 +11,25 @@ $aliases['dev'] = array(
 
 	'ssh-options' => "-p 37241",
 );
+
+$aliases['staging'] = array(
+	'uri'=> 'whiwh.ccistaging.com',
+	'root' => '/home/staging/subdomains/whiwh/public_html',
+	'remote-host'=> 'host.ccistudios.com',
+	'remote-user'=> 'staging',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	),
+
+	'ssh-options' => "-p 37241",
+);
+
+$aliases['dev2'] = array(
+	'uri'=> 'dev2.whiwh.com',
+	'root' => '/home/whiwh/subdomains/dev2/public_html',
+	'remote-host'=> 'host.lambtonshield.com',
+	'remote-user'=> 'whiwh',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	)
+);
